0141193da8d766f4e0fcabae8a175cf39ef91917
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2019-05-09 09:42:42 +0200

    Conexión a DB

--- src/sqlitejdbc/SQLiteJDBC.java
+++ src/sqlitejdbc/SQLiteJDBC.java
@@ -18,36 +18,14 @@ public class SQLiteJDBC {
      /**
      * Conectar a DB, haciendo clase interna
      */
-    public static void connect() {
-        
-        Connection conn = null;
-        try {
-            // parámetro DB
-            String url = "jdbc:sqlite:/home/local/DANIELCASTELAO/ecollazodominguez/NetBeansProjects/SQLiteJDBC/db/chinook.db";
-            // Creando conexión a la DB
-            conn = DriverManager.getConnection(url);
-            
-            System.out.println("La conexión a SQLite ha sido establecida");
-            
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //conecta
-        connect();
+        Métodos met = new Métodos();
+        met.connect();
+
     }
     
 }
